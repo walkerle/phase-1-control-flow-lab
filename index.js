aacfe146384024715829cc4203adf4424f27bc08
Walker Le <walker.le88@gmail.com>
2022-12-14 21:06:12 -0700

    commit tree 9e7427c4c343c269fb32f98e1d0e1c409ba470d1

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
+  let message = "";
+  if (distance < 400) {
+    message = 'This one is on me!';
+  } else if (distance >= 400 && distance <= 2000) {
+    message = 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <=2500) {
+    message = 'I will gladly take your thirty bucks.';
+  } else {
+    message = 'No can do.';
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let message;
+  return (city == 'NYC' ? (message = 'Ok, sounds good.') : (message = 'No go.'));
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let message;
+  switch (tip) {
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message ='Bye.'
+  }
+  return message;
 }
